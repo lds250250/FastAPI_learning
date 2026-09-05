c6ca4e0a30a3b9c81b547c6e9d7d7cd9c814792b
Lcloudy <306733994@qq.com>
2026-09-05 21:50:34 +0800

    更新1.5的内容

--- main.py
+++ main.py
@@ -1,28 +1,110 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic import Field
+from fastapi import status
+from fastapi import HTTPException
 
 app = FastAPI()
 
-#13
+
+class UserResponse(BaseModel):
+    username: str = Field(description="用户名")
+    email: str = Field(description="邮箱")
+
+
+fake_db_user = {
+    "username": "cloudy",
+    "email": "cloudy@example.com",
+    "password": "super_secret_123",
+    "internal_status": "active_admin"
+}
+
+
+class Publisher(BaseModel):
+    name: str = Field(..., min_length=2, max_length=50, description="出版社名称")
+    address: str | None = Field(
+        default=None, max_length=200, description="出版社地址")
+
 # 1. 定义 Pydantic 数据模型
+
+
 class BookCreate(BaseModel):
-    title: str
-    author: str
-    price: float
-    description: str | None = None
+    title: str = Field(
+        ...,
+        min_length=2,
+        max_length=100,
+        description="书籍标题，长度2-100",
+        examples=["三体"]
+    )
+    author: str = Field(
+        ...,
+        min_length=2,
+        max_length=50,
+        description="作者姓名"
+    )
+    price: float | None = Field(
+        default=None,
+        gt=0,
+        le=9999.99,
+        description="价格必须大于0且不超过9999.99"
+    )
+    publisher: Publisher | None = Field(default=None, description="出版社信息（可选）")
+
+
+fake_db = {
+    1: {"id": 1, "title": "三体", "author": "刘慈欣"},
+    2: {"id": 2, "title": "活着", "author": "余华"}
+}
 
 
 # 2. 将模型作为函数参数（FastAPI 自动识别为请求体）
-@app.post("/books/")
+@app.post(
+    "/books/",
+    tags=["书籍管理"],
+    summary="创建一本新书",
+    status_code=status.HTTP_201_CREATED
+)
 async def create_book(book: BookCreate):
+    new_id = max(fake_db.keys())+1 if fake_db else 1
+    new_book = {"id": new_id, **book.model_dump()}
+    fake_db[new_id] = new_book
     return {
         "message": "书籍创建成功",
         "book_title": book.title,
-        "book_price": book.price,
     }
 
 
+@app.get(
+    "/books/{book_id}",
+    tags=["书籍管理"],
+    summary="根据ID获取书籍详情"
+)
+async def get_book(book_id: int):
+    if book_id not in fake_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"找不到 ID 为 {book_id} 的书籍"
+        )
+    return fake_db[book_id]
+
+
+@app.delete(
+        "/books/{book_id}",
+        tags=["书籍管理"],
+        summary="删除指定书籍", 
+        status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    if book_id not in fake_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"无法删除：找不到 ID 为 {book_id} 的书籍"
+        )
+    del fake_db[book_id]
+    return
+
 # 接口1：根路径
+
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
